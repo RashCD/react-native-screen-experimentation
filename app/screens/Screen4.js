a94eddab617498ed0dbcee37eac98e4e6ba65704
RashCD <mrrash91@gmail.com>
2017-08-21 18:07:45 +0800

    adjust screen 4 for triangle background and shadow UI. A few UI changes

--- app/screens/Screen4.js
+++ app/screens/Screen4.js
@@ -3,60 +3,129 @@ import {
   View,
   Text,
   StyleSheet,
-  Image 
+  Image,
+  TouchableOpacity, 
+  Platform,
+  Dimensions,
 } from 'react-native';
 
 const testPicture = require('../images/people.jpg');
 const testPicturePotrait = require('../images/potrait.jpg');
 
+const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
+
 class Screen extends Component {
-  
+
+  static navigatorStyle = {
+    ...Platform.select({
+        ios: {
+            drawUnderNavBar: true,
+            navBarTranslucent: true,
+            navBarTransparent: true,
+            tabBarHidden: true,
+            navBarTextColor: 'white'
+        },
+        android: {
+            drawUnderNavBar: true,
+            navBarTransparent: true,
+            tabBarHidden: true,
+        },
+    }),
+  };
+
   render() {
     const { 
       container,  
       imageTopSection, 
       iconFloat,
       circleIcon,
+      floatCardContainer,
+      floatCard,
       triangleBackground,
       topPart,
       middlePart,
-      endPart
+      endPart,
+      clickableButtonContainer,
+      clickableButton,
+      cardShadow,
+      borderDev
     } = styles;
+
+    const {
+      textBasic,
+      textBold,
+    } = basic;
 
     return (
       <View style={container}>
-        <View style={topPart}>
-          <Image source={testPicture} style={imageTopSection} />
-          <View style={triangleBackground} />
-          <View style={iconFloat}>
-            <Image 
-              style={circleIcon} 
-              source={testPicturePotrait} 
-              borderRadius={50} 
-              resizeMode="cover" 
-            />
+        <Image source={testPicture} style={imageTopSection} >
+          <View style={[floatCardContainer]}>
+
+            <View style={[floatCard, cardShadow]}>
+              <View style={[triangleBackground, cardShadow]} />
+              <View style={middlePart} />
+                <View style={[endPart]}>
+                  <Text style={[textBold, { paddingLeft: 15 }]}> Susan Ledger </Text>
+                  <Text style={[textBasic, { paddingLeft: 25 }]}> Writer, Curator, Fashion, Researcher </Text>
+
+                  <View style={clickableButtonContainer}>
+                    <TouchableOpacity style={[clickableButton]}> 
+                      <Text style={textBasic}> + Add </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[clickableButton]} >
+                      <Text style={textBasic}> Message </Text>
+                    </TouchableOpacity>
+                  </View>
+
+                  <View style={[{ justifyContent: 'center', alignItems: 'center', marginTop: 10, }]}>
+                    <View style={[{ width: '90%', height: 40, justifyContent: 'center', borderTopWidth: 1, borderBottomWidth: 1, borderColor: 'lightgrey' }]}>
+                      <Text style={[textBasic, { textAlign: 'center' }]}> Mutual Friends </Text>
+                    </View>
+                  </View>
+
+                </View>
+            </View>
+            <View style={iconFloat}>
+                <Image 
+                  style={circleIcon} 
+                  source={testPicturePotrait} 
+                  borderRadius={50} 
+                  resizeMode="cover" 
+                />
+            </View>
           </View>
-        </View> 
-        
-        <View style={middlePart} />
-        <View style={endPart} >
-          <Text style={{ paddingLeft: 15, fontSize: 25, fontWeight: 'bold', color: 'black' }}> Susan Ledger </Text>
-          <Text style={{ paddingLeft: 25, fontSize: 15, fontWeight: 'normal', color: 'black' }}> Writer, Curator, Fashion, Researcher </Text>
-        </View>
-        
+        </Image>
       </View>
     );
   }
 }
 
+const basic = {
+  textBasic: {
+    fontSize: 15,
+    fontWeight: 'normal',
+    color: 'black',
+  },
+  textBold: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'black',
+  }
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // backgroundColor: 'white'
     // alignItems: 'center'
   },
   imageTopSection: {
+    // flex: 1,
     height: '100%',
-    width: '100%'
+    width: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
   },
   topPart: {
     flex: 2,
@@ -65,34 +134,93 @@ const styles = StyleSheet.create({
   },
   iconFloat: {
     position: 'absolute',
-    left: 20,
-    top: 180,
-    
+    left: 40,
+    top: 30,
+
+    ...Platform.select({
+      android: {
+        elevation: 10,
+      }
+    })
   },
   circleIcon: {
     height: 100,
     width: 100,
     borderRadius: 50,
     borderWidth: 1,
-    borderColor: 'blue',
+    borderColor: 'black',
+  },
+  floatCardContainer: {
+    // backgroundColor: 'white',
+    height: 400,
+    width: '100%',
+    // marginLeft: 20,
+    // marginRight: 20,
+    // marginBottom: 20,
+  },
+  floatCard: {
+    // backgroundColor: 'white',
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 20,
   },
   triangleBackground: {
-    position: 'absolute',
-    bottom: 0,
-    borderBottomWidth: 150,
+    width: '100%',
+    height: 0,
+    borderBottomWidth: 100,
     borderBottomColor: 'white',
-    borderLeftWidth: 450,
+    borderLeftWidth: (screenWidth - 40),
     borderLeftColor: 'transparent',
   },
   middlePart: {
-    flex: 0.1,
-    backgroundColor: 'transparent',
+    height: 40,
+    backgroundColor: 'white',
   },
   endPart: {
-    flex: 2,
     width: '100%',
+    height: '100%',
     backgroundColor: 'white',
-  }
+  },
+  clickableButtonContainer: {
+    // alignItems: 'center',
+    flexDirection: 'row', 
+    width: '100%', 
+    justifyContent: 'space-around', 
+    marginTop: 15,
+  },
+  clickableButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 150,
+    height: 30,
+    borderRadius: 5,
+    backgroundColor: 'grey'
+  },
+
+  cardShadow: {
+    ...Platform.select({
+        ios: {
+            shadowRadius: 4,
+            shadowOffset: {
+                width: 0, 
+                height: 1,
+            },
+            shadowOpacity: 0.5,
+            shadowColor: 'black',
+            backgroundColor: 'transparent',
+        },
+        android: {
+            // for android version 5.0+ only
+            // backgroundColor: 'white',
+            // elevation: 1,
+        },
+    }),
+    
+  },
+  borderDev: {
+    borderWidth: 1,
+    borderColor: 'black',
+},
 
 });
 
