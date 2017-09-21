7212bf0af4e199107f3685959e98e971fb1c4a21
Gorgias <Gorgias@Macs-MacBook-Pro.local>
2017-09-21 11:53:47 +0800

    add a few more screen

--- app/index.js
+++ app/index.js
@@ -14,6 +14,8 @@ import Screen11 from './screens/ScreenUserProfileStraight';
 import Screen12 from './screens/ScreenCamera';
 import Screen13 from './screens/ScreenSharedElement';
 import Screen14 from './screens/ScreenSharedElement2';
+import Screen15 from './screens/ScreenTransitionTest';
+import Screen16 from './screens/ScreenTransitionTest2';
 
 const iconNormal = require('./images/icon1.png');
 const iconHighlight = require('./images/icon1_selected.png');
@@ -35,12 +37,14 @@ export default () => {
   Navigation.registerComponent('Screen12', () => Screen12);
   Navigation.registerComponent('Screen13', () => Screen13);
   Navigation.registerComponent('Screen14', () => Screen14);
+  Navigation.registerComponent('Screen15', () => Screen15);
+  Navigation.registerComponent('Screen16', () => Screen16);
 
   Navigation.startTabBasedApp({
     tabs: [
       {
         label: 'One',
-        screen: 'Screen13',
+        screen: 'Screen15',
         icon: iconNormal,
         selectedIcon: iconHighlight,
         title: 'Screen One'
